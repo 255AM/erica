e87f4f1e150cc3ace83814a63cb23987d891c91b
unknown <clane@CLANE7250W7.cbsinteractive.net>
2015-08-01 17:25:44 -0400

    Recreated carousel using manual Jquery call for demonstration Changes to styling

--- js/carousel.js
+++ js/carousel.js
@@ -1,1 +1,53 @@
-$("#carousel").carousel();
+ 
+
+ 
+$(document).ready(function(){
+    // Activate Carousel
+    $("portfoliocarousel").carousel();
+    
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#portfoliocarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#portfoliocarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#portfoliocarousel").carousel(2);
+    });
+    $(".item4").click(function(){
+        $("#portfoliocarousel").carousel(3);
+    });
+    $(".item5").click(function(){
+        $("#portfoliocarousel").carousel(4);
+    });
+    $(".item6").click(function(){
+        $("#portfoliocarousel").carousel(5);
+    });
+    $(".item7").click(function(){
+        $("#portfoliocarousel").carousel(6);
+    });
+    $(".item8").click(function(){
+        $("#portfoliocarousel").carousel(7);
+    });
+    $(".item9").click(function(){
+        $("#portfoliocarousel").carousel(8);
+    });
+    
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#portfoliocarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#portfoliocarousel").carousel("next");
+    });
+});
+ 
+
+ 
+
+ 
+ 
+
+
+ 
